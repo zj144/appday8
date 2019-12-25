d64f70ac31d986e6db5b40dfdaef41b768a7136f
zj144 <16600282792@163.com>
2019-12-25 20:50:37 +0800

    asc

--- scripts/09test_lx.py
+++ scripts/09test_lx.py
@@ -1,3 +1,25 @@
+import allure
+import pytest
+
+
 class Test_003:
-    def test_003_1(self):
+
+    # @allure.step("这是一个测试步骤~")
+    # def test_003_1(self):
+    #     allure.attach("文件名称","文件内容，具体步骤描述信息")
+    #     assert True
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test_01(self):
+        assert True
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_02(self):
+        assert True
+    @pytest.allure.severity(pytest.allure.severity_level.NORMAL)
+    def test_03(self):
+        assert True
+    @pytest.allure.severity(pytest.allure.severity_level.MINOR)
+    def test_04(self):
+        assert False
+    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    def test_05(self):
         assert True
